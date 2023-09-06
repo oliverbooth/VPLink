d2613dbdcb49e89349fc4523de348fa1036a080c
Oliver Booth <git@olivr.me>
2023-09-06 23:48:00 +0100

    refactor: rename pencilVersion to botVersion

--- VPLink/Commands/InfoCommand.cs
+++ VPLink/Commands/InfoCommand.cs
@@ -30,7 +30,7 @@ internal sealed class InfoCommand : InteractionModuleBase<SocketInteractionConte
     public async Task InfoAsync()
     {
         SocketGuildUser member = Context.Guild.GetUser(_discordClient.CurrentUser.Id);
-        string pencilVersion = _botService.Version;
+        string botVersion = _botService.Version;
 
         SocketRole? highestRole = member.Roles.Where(r => r.Color != Color.Default).MaxBy(r => r.Position);
 
@@ -38,13 +38,13 @@ internal sealed class InfoCommand : InteractionModuleBase<SocketInteractionConte
         embed.WithAuthor(member);
         embed.WithColor(highestRole?.Color ?? Color.Default);
         embed.WithThumbnailUrl(member.GetAvatarUrl());
-        embed.WithTitle($"VPLink v{pencilVersion}");
+        embed.WithTitle($"VPLink v{botVersion}");
         embed.WithDescription("Created by <@94248427663130624>, hosted [on GitHub](https://github.com/oliverbooth/VPLink).");
         embed.AddField("Ping", $"{_discordClient.Latency} ms", true);
         embed.AddField("Started", $"<t:{_botService.StartedAt.ToUnixTimeSeconds()}:R>", true);
 
         var builder = new StringBuilder();
-        builder.AppendLine($"VPLink: {pencilVersion}");
+        builder.AppendLine($"VPLink: {botVersion}");
         builder.AppendLine($"Discord.Net: {_botService.DiscordNetVersion}");
         builder.AppendLine($"VP#: {_botService.VpSharpVersion}");
         builder.AppendLine($"CLR: {Environment.Version.ToString(3)}");
